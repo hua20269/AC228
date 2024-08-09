7fd122c4002f43783f7ae79dbd1b661460539e93
1846328914@qq.com <1846328914@qq.com>
2024-08-09 16:32:01 +0800

    .

---
+++ lib/OTA/ota.h
@@ -0,0 +1,13 @@
+#ifndef _OTA_H
+#define _OTA_H
+
+#define wifi_SSID "2024"     // WIFI名称，区分大小写，不要写错
+#define wifi_PASS "12345678" // WIFI密码
+
+extern int a, b;
+
+#define upUrl "http://bin.bemfa.com/b/3BcYWMzNDRjODg3OGFiNWQwMWNlNDFkMjNiMmRjZjRmNzQ=AC228.bin"
+
+void updateBin(); // wifiManager配网      httpUpdate OTA更新
+
+#endif
